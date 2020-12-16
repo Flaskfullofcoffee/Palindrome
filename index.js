d49930401c5612634643f5059ce5449348048652
Flaskfullofcoffee <zdobbs3@gmail.com>
2020-12-16 10:16:59 -0700

    Initialized repo

---
+++ index.js
@@ -0,0 +1,35 @@
+//// WRITING A fUNCTION THAT CAN DETECT PALINDROMES
+
+// Reverses a string
+function reversify(str) {
+  return Array.from(str).reverse().join('');
+}
+// console.log(reversify(`Racecar`));
+
+
+// Defines a Phrase object
+function Phrase(content) {
+  this.content = content;
+
+    this.processedContent = function processedContent() {
+      return this.content.toLowerCase();
+    }
+
+    this.isPalindrome = function isPalindrome() {
+      return this.processedContent() === reversify(this.processedContent());
+    }
+}
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  this.processedContent = function processedContent() {
+    return this.translation.toLowerCase();
+  }
+}
+TranslatedPhrase.prototype = new Phrase();
+
+String.prototype.reverse = function () {
+  return Array.from(this).reverse().join('');
+};
